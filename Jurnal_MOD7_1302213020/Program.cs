c9dde8f826718351b9d7b88fe077c94ed6f54228
Arya Ashari <aryaashari100@gmail.com>
2023-04-10 06:55:23 +0700

    initital commit

---
+++ Jurnal_MOD7_1302213020/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD7_1302213020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
