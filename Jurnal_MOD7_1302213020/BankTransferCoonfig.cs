72bf110aa13986e9bfafd5790a8d2108de73d7b6
Arya Ashari <aryaashari100@gmail.com>
2023-04-10 07:58:59 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ Jurnal_MOD7_1302213020/BankTransferCoonfig.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD7_1302213020
+{
+    internal class BankTransferCoonfig
+    {
+
+        public Config config;
+
+        public const String filePath = @"bank_transfer_config.jspn";
+
+
+        private Config ReadConfigFile()
+        {
+            String configJson = File.ReadAllText(filePath);
+            config = JsonSerializer.Deserialize<Config>(configJson);
+
+        }
+
+        private void WriteNewConfigFile()
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true;
+            };
+            String jsonString = JsonSerializer.Serialize(config, options);
+            File.WriteAllText(filepath, jsonString);
+        }
+
+        public BankTransferCoonfig()
+        {
+            try
+            {
+                ReadConfigFile();
+            } cactch (Exception) {
+                SetDefault();
+                WriteConfigFile();
+            }
+        }
+
+}
+}
---
+++ Jurnal_MOD7_1302213020/Config.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jurnal_MOD7_1302213020
+{
+    internal class Config
+    {
+
+        public string lang { get; set; }
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+        public int high_fee { get; set; }
+        public string[] methods;
+        public string en;
+        public string id;
+
+        public Config() { }
+
+        public Config(string lang, int threshold, int low_fee, int high_fee, string[] methods, string en, string id)
+        {
+            this.lang = lang;
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+            this.methods = methods;
+            this.en = en;
+            this.id = id;
+        }
+    }
+}
